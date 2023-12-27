673397df2379f65c1eef5b04eae456f32cfcf8b0
jimmywu88 <94825422+jimmywu88@users.noreply.github.com>
2023-12-27 16:49:14 +0800

    Update server.js add backgroud color

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ response.writeHead(200);
 response.end("Hello World! SS2 OCP v0");
 }
 var www = http.createServer(handleRequest);
+window.addEventListener('load', () => {
+  document.body.style.backgroundColor = 'green';
+});
 www.listen(8080);
