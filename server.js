3b41227eb08b1beee6c8a60a741f71ee69736484
jimmywu88 <94825422+jimmywu88@users.noreply.github.com>
2023-12-27 16:56:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
 response.writeHead(200);
-response.end("Hello World! SS2 OCP v0");
+response.end("Hello World! SS2 OCP v1");
 }
 var www = http.createServer(handleRequest);
-window.addEventListener('load', () => {
-  document.body.style.backgroundColor = 'green';
-});
 www.listen(8080);
