e6530e8862c81fd5ec94d4e2b105655662a529ea
jimmywu88 <94825422+jimmywu88@users.noreply.github.com>
2024-11-26 18:46:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
 response.writeHead(200);
-response.end("Hello World! SS2 OCP v20241017");
+response.end("Hello World! SS2 OCP v20241126");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
